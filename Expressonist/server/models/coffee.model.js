5e9a722e3719488014639a33255367fb51879d78
eoinmoney78 <94717367+eoinmoney78@users.noreply.github.com>
2023-03-22 20:45:01 -0400

    eoin-push-schema

--- Expressonist/server/models/coffee.model.js
+++ Expressonist/server/models/coffee.model.js
@@ -1,3 +1,22 @@
 const mongoose = require('mongoose');
 
+const UserSchema = new mongoose.Schema({
+    roaster: { type: String, required: true },
+    coffee: { type: String, required: true },
+    process: { type: String, required: true },
+    variety: String,
+    elevation: Number,
+    roast: String,
+    in: { type: Number, required: true },
+    out: { type: Number, required: true },
+    time: { type: Number, required: true },
+    grind: { type: Number, required: true },
+    temp: { type: Number, required: true },
+    wedge: Number,
+    wdt: { type: Boolean, required: true },
+    rdt: { type: Boolean, required: true },
+    notes: String,
+    img: String
+});
+
 module.exports = mongoose.model('User', UserSchema);
