cb5340c7d7978cc674edd1d764ac4e62a2e04c69
eoinmoney78 <94717367+eoinmoney78@users.noreply.github.com>
2023-03-30 16:26:41 -0400

    eoin-dashboard-with-entries

--- Expressonist/client/src/components/auth/logout/Logout.jsx
+++ Expressonist/client/src/components/auth/logout/Logout.jsx
@@ -28,17 +28,22 @@ import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/system';
 
-const LogoutButton = styled(Button)(({ theme }) => ({
-    backgroundColor: '#f44336', // Red color
-    color: '#ffffff', // White color
-    position: 'absolute',
-    top: theme.spacing(1),
-    right: theme.spacing(1),
-    '&:hover': {
-      backgroundColor: '#d32f2f', // Darker red color on hover
-    },
-  }));
+
   
+
+const LogoutButton = styled(Button)(({ theme }) => ({
+  backgroundColor: '#f44336', // Red color
+  color: '#ffffff', // White color
+  position: 'absolute',
+  top: theme.spacing(1),
+  right: theme.spacing(1),
+  '&:hover': {
+    backgroundColor: '#d32f2f', // Darker red color on hover
+  },
+  fontSize: '0.8rem', // reduce font size
+  opacity: 0.7, // reduce opacity
+}));
+
 
 function Logout({ setToken }) {
   const navigate = useNavigate();
