a479ec2fc31672354eecf3f70d53d646f308afad
starvingzeta <benyaminrivera0@gmail.com>
2023-04-08 16:05:10 -0400

    homepage

--- Expressonist/client/src/components/dashboard/Dashboard.jsx
+++ Expressonist/client/src/components/dashboard/Dashboard.jsx
@@ -5,16 +5,16 @@ import { baseURL } from '../../environment';
 import CoffeeDetails from '../coffee/CoffeeDetails';
 import TemporaryDrawer from '../layout/TemporaryDrawer';
 
+
+
 // The Component Dashboard  sets the initial state of coffeeEntries as an empty array, and userId as the user_id value  from local storage.
 const Dashboard = () => {
   const [coffeeEntries, setCoffeeEntries] = useState([]);
   const [userId] = useState(localStorage.getItem('user_id'));
 
-  console.log("UserID:", userId)
 
   // With FetchCoffeeEntries the useCallBack was added due to the coffees keep rendering on the console  so this way the it doesn't get recreated on every render.
 
-<<<<<<< HEAD
 
   
   const fetchCoffeeEntries = useCallback(async () => {
@@ -68,12 +68,8 @@ const Dashboard = () => {
 /*   const fetchCoffeeEntries = useCallback(async () => {
   const url = `${baseURL}/coffee/getall/${userId}`;
   
-=======
-  const fetchCoffeeEntries = useCallback(async () => {
-    const url = `${baseURL}/coffee/getall/`;
-    
->>>>>>> e71f85563f7f600cd45aa57e2eddc019a2f5f2f3
     try {
+      // GET request to the  endpoint for fetching all coffee entries associated with a particular user. The user ID isin the userId state above.
       const response = await fetch(url, {
         headers: new Headers({
           'Authorization': `${localStorage.getItem('token')}`,
@@ -84,32 +80,37 @@ const Dashboard = () => {
         const errorData = await response.json();
         throw new Error(errorData.message || 'Failed to fetch coffee entries');
       }
-  
+
       const data = await response.json();
+//  if successful, the function uses await to parse the response data as JSON and logs it to the console.
       console.log('Fetched coffee entries:', data);
-  
+
+//  If the data includes an array of coffee entries, the function filters this array to include only entries with a userId value that matches the current user ID, 
       if (data && Array.isArray(data.coffeeEntries)) {
-        setCoffeeEntries(data.coffeeEntries);
+        const filteredData = data.coffeeEntries.filter(entry => String(entry.userId)
+          === String(localStorage.getItem('user_id')));
+
+        console.log('Filtered coffee entries:', filteredData);
+        // update dashboard with the fetched filtered data
+        setCoffeeEntries(filteredData);
       } else {
         console.error('Error fetching coffee entries: data.coffeeEntries is not an array');
       }
     } catch (error) {
       console.error('Error fetching coffee entries:', error.message);
     }
-<<<<<<< HEAD
   }, [userId]); */
 
-=======
-  }, []);
-  
-  
->>>>>>> e71f85563f7f600cd45aa57e2eddc019a2f5f2f3
 
   // this calls the fetchCoffeeEntries function when the component mounts and whenever fetchCoffeeEntries or userId changes.
   useEffect(() => {
     fetchCoffeeEntries();
-  }, [fetchCoffeeEntries]);
+  }, [fetchCoffeeEntries, userId]);
+
+
+
 // sends a DELETE request to the API to delete a coffee entry with the given ID, and removes the deleted entry from the state of coffee entries if successful.
+
   
   const handleDeleteCoffee = async (id) => {
     const url = `${baseURL}/coffee/${id}`;
@@ -146,13 +147,6 @@ const Dashboard = () => {
         Dashboard
       </Typography>
       <div>
-<<<<<<< HEAD
-=======
-        <br />
-        <br />
-        <br />
-        <br />
->>>>>>> e71f85563f7f600cd45aa57e2eddc019a2f5f2f3
 
         {/* checks if there are any entries and if there are, it maps over the array and makes a div element for each coffee , they have a unique key basedon the coffe enrty's id*/}
 
@@ -172,7 +166,6 @@ const Dashboard = () => {
               <Link to={`/edit-coffee/${coffee._id}`}>
                 <button>Edit</button>
               </Link>
-              
 
               <hr />
             </div>
@@ -182,7 +175,15 @@ const Dashboard = () => {
         )}
       </div>
     </Container>
-      );
+  
+
+
+
+    );
     };
     
     export default Dashboard;
+
+
+
+
