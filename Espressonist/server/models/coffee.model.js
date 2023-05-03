3fd195a4268147925f2c0e67b1b443b93d2ddaed
eoinmoney78 <94717367+eoinmoney78@users.noreply.github.com>
2023-05-02 20:10:29 -0400

    star-rating-server

--- Espressonist/server/models/coffee.model.js
+++ Espressonist/server/models/coffee.model.js
@@ -1,7 +1,6 @@
-
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
+const CoffeeSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     roaster: { type: String, required: true },
     coffee: { type: String, required: true },
@@ -19,6 +18,31 @@ const UserSchema = new mongoose.Schema({
     rdt: { type: Boolean, required: true },
     notes: String,
     img: String,
+    rating: { type: Number, required: false },
 });
 
-module.exports = mongoose.model('Coffee', UserSchema);
+module.exports = mongoose.model('Coffee', CoffeeSchema);
+
+// const mongoose = require('mongoose');
+
+// const UserSchema = new mongoose.Schema({
+//     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+//     roaster: { type: String, required: true },
+//     coffee: { type: String, required: true },
+//     process: { type: String, required: true },
+//     variety: String,
+//     elevation: Number,
+//     roast: String,
+//     in: { type: Number, required: true },
+//     out: { type: Number, required: true },
+//     time: { type: Number, required: true },
+//     grind: { type: Number, required: true },
+//     temp: { type: Number, required: true },
+//     wedge: Number,
+//     wdt: { type: Boolean, required: true },
+//     rdt: { type: Boolean, required: true },
+//     notes: String,
+//     img: String,
+// });
+
+// module.exports = mongoose.model('Coffee', UserSchema);
